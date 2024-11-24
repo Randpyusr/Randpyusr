acad8608c8b8d826d51250d5fc708c88b71ea7da
Randpyusr <160542126+Randpyusr@users.noreply.github.com>
2024-11-24 13:40:12 +0100

    Create cards.py to store cards that will be used in the game later

---
+++ cards.py
@@ -0,0 +1,16 @@
+import methods
+# cartes de healing
+soins20 = methods.Object(20, 0, 0)
+soins30 = methods.Object(30, 0, 0)
+soins40 = methods.Object(40, 0, 0)
+soins50 = methods.Object(50, 0, 0)
+# cartes de strenght
+strenght20 = methods.Object(0, 20, 0)
+strenght30 = methods.Object(0, 30, 0)
+strenght40 = methods.Object(0, 40, 0)
+strenght50 = methods.Object(0, 50, 0)
+# cartes d'endurance
+endurance2 = methods.Object(0, 0, 2)
+endurance3 = methods.Object(0, 0, 3)
+endurance4 = methods.Object(0, 0, 4)
+endurance5 = methods.Object(0, 0, 5)
