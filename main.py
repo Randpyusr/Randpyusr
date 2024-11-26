e875c408a415e44fb0f6fd7d8b3abab9574580a6
Randpyusr <160542126+Randpyusr@users.noreply.github.com>
2024-11-26 21:22:08 +0100

    nothing great here /:

--- main.py
+++ main.py
@@ -1,18 +1,8 @@
 import methods
-# import cards inutilisé pour le moment 
-"test de la fonction avec pierre attaque caillou"
-methods.set_name('kit1', methods.kit_list, 'carcaters')
-methods.set_name('kit1', methods.kit_list, 'carcaters')
-methods.printing_status(methods.caracters,'pierre')
-methods.printing_status(methods.caracters,'caillou')
-methods.attack(methods.caracters['pierre'],methods.caracters['caillou'])
-methods.printing_status(methods.caracters,'pierre')
-methods.printing_status(methods.caracters,'caillou')
-# Test de la fonction carcter_change pour changer de personnage actif
+# Test of the function character change
 methods.set_name('kit1', methods.kit_list)
 methods.set_name('kit1', methods.kit_list)
 print(methods.currently_selected)
-methods.caracter_change()
+methods.character_change()
 print(methods.currently_selected)
-print(methods.caracters)
-# Rien à faire dans le main pour le moment, création des cartes et des fonctions avant
+print(methods.characters)
