107ede4fa3cd35262bd800eaa57d2a286b018732
Randpyusr <160542126+Randpyusr@users.noreply.github.com>
2024-11-26 20:47:40 +0100

    New cards now added still have code to update in the methods.py to go with the new cards system

--- cards.py
+++ cards.py
@@ -1,16 +1,31 @@
 import methods
-# cartes de healing
-soins20 = methods.Object(20, 0, 0)
-soins30 = methods.Object(30, 0, 0)
-soins40 = methods.Object(40, 0, 0)
-soins50 = methods.Object(50, 0, 0)
-# cartes de strenght
-strenght20 = methods.Object(0, 20, 0)
-strenght30 = methods.Object(0, 30, 0)
-strenght40 = methods.Object(0, 40, 0)
-strenght50 = methods.Object(0, 50, 0)
-# cartes d'endurance
+
+# healing cards
+heal20 = methods.Object(20, 0, 0)
+heal30 = methods.Object(30, 0, 0)
+heal40 = methods.Object(40, 0, 0)
+heal50 = methods.Object(50, 0, 0)
+
+# strength cards
+strength20 = methods.Object(0, 20, 0)
+strength30 = methods.Object(0, 30, 0)
+strength40 = methods.Object(0, 40, 0)
+strength50 = methods.Object(0, 50, 0)
+
+# endurance cards
 endurance2 = methods.Object(0, 0, 2)
 endurance3 = methods.Object(0, 0, 3)
 endurance4 = methods.Object(0, 0, 4)
 endurance5 = methods.Object(0, 0, 5)
+
+# playing cards
+Eclipse = methods.Hero('Eclipse', 15, 73, 10)
+Ironclad = methods.Hero("Ironclad", 12, 88, 7)
+BlazeFury = methods.Hero("BlazeFury", 18, 64, 14)
+Guardian = methods.Hero("Guardian", 10, 92, 6)
+ShardSpire = methods.Hero("ShardSpire", 20, 55, 12)
+JadeFang = methods.Hero("JadeFang", 13, 81, 9)
+Dreadnought = methods.Hero("Dreadnought", 14, 75, 11)
+Aegis = methods.Hero("Aegis", 11, 90, 5)
+Havoc = methods.Hero("Havoc", 19, 50, 15)
+FrostBlade = methods.Hero("FrostBlade", 16, 66, 13)
